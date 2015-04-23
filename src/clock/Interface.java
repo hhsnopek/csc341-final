1a1c347a8bec69e11b1e58105f71282654db83a9
Henry Snopek <hhsnopek@gmail.com>
2015-04-22 22:19:02 -0500

    initial scaffolding

--- src/clock/Interface.java
+++ src/clock/Interface.java
@@ -1,5 +1,30 @@
 package clock;
 
+import Controller;
+
 public class Interface {
 
+  public void setAlarm(Date date) {
+
+  }
+}
+
+class Button {
+  private String name = null;
+
+  public Button(String name) {
+    this.name = name;
+
+    switch (name) {
+      case "set": {
+        // toggle style
+      }
+      case "sleep": {
+        // toggle style
+      }
+      case "on/off": {
+        // toggle style
+      }
+    }
+  }
 }
