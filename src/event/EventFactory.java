b4480581a820c3de628561a3cb2ce4b71288bcaa
Henry Snopek <hhsnopek@gmail.com>
2015-04-25 18:26:14 -0500

    add type parameter to specify Yellow/Red Alert

--- src/event/EventFactory.java
+++ src/event/EventFactory.java
@@ -3,14 +3,14 @@ package event;
 import java.util.Date;
 
 public class EventFactory {
-  public Event getEvent(String type, Date time, String action, String entity) {
+  public Event getEvent(String type, Date time, String action, String entity, String alertType) {
     if (type == null) return null;
 
     if(type.equalsIgnoreCase("alarm")){
        return new Alarm(time, action, entity);
 
     } else if(type.equalsIgnoreCase("alert")){
-       return new Alert(time, action, entity);
+       return new Alert(time, action, entity, alertType);
 
     } else if(type.equalsIgnoreCase("reminder")){
        return new Reminder(time, action, entity);
